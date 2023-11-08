238411b92b37d9067462282e78a1669cc606b1e9
chancealbo <150288564+chancealbo@users.noreply.github.com>
2023-11-08 11:23:47 -0800

    Update streamlit_app.py
    made better menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents new healthy diner')
 
-streamlit.header('breakfast menu')
-streamlit.text('oatmeal')
-streamlit.text('smoothy')
-streamlit.text('egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avamacado toasties')
+   
