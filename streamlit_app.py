0bf72862ab756af897aba31c882d54242053e2fc
chancealbo <150288564+chancealbo@users.noreply.github.com>
2023-11-08 11:21:05 -0800

    Update streamlit_app.py
    added breakfast header
    added texts for breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
+
+streamlit.header('breakfast menu')
+streamlit.text('oatmeal')
+streamlit.text('smoothy')
+streamlit.text('egg')
