57762d094baabed32dda213e0119eb1fe5d5282e
chancealbo <150288564+chancealbo@users.noreply.github.com>
2023-11-16 12:01:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'brown')
 # sql_exec = my_cur.execute("INSERT INTO fruit_load_list VALUES " + 
-    "    ('{}') ".format(add_my_fruit))
+#    "    ('{}') ".format(add_my_fruit))
 
 
 streamlit.button("Add Fruit", on_click=(streamlit.write('The user entered ', add_my_fruit)))
